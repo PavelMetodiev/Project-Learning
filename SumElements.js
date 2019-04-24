c03f108f04a01902f0668c01dac8769c4cf1c233
PavelMetodiev <48319741+PavelMetodiev@users.noreply.github.com>
2019-04-24 15:53:20 +0300

    Create SumElements.js

---
+++ SumElements.js
@@ -0,0 +1,12 @@
+function solve(input) {
+    
+    let numOne = input[0]
+    let numTwo = input[input.length - 1]
+    
+    let result = +numOne + +numTwo
+    
+    console.log(result)
+    
+}    
+    
+solve(['10', '17', '22', '33']) 
