773806c29cb1edab6f2b579ebe752f73c69f9be8
PavelMetodiev <48319741+PavelMetodiev@users.noreply.github.com>
2019-05-03 11:45:12 +0300

    Create smalestOfThree.js

---
+++ Basicexercise/smalestOfThree.js
@@ -0,0 +1,8 @@
+function smalestOfThree(numOne, numTwo, numThree) {
+	if (numOne < numTwo && numOne < numThree) {    
+    	console.log(numOne)	
+    } else if (numOne > numTwo && numTwo < numThree) {        	console.log(numTwo)	
+    } else {
+    	console.log(numThree)	
+    }
+}	
