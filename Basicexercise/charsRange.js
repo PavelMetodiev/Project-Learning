8559e65d6f8ad470aff68d585fe15f657a940402
PavelMetodiev <48319741+PavelMetodiev@users.noreply.github.com>
2019-05-03 16:33:16 +0300

    Update charsRange.js

--- Basicexercise/charsRange.js
+++ Basicexercise/charsRange.js
@@ -2,19 +2,21 @@ function charRange(charOne, charTwo) {
     let asciiOne = charOne.charCodeAt(0)
     let asciiTwo = charTwo.charCodeAt(0)
     let result = ""
+    let arr = []
     
     if (asciiOne < asciiTwo) {
     	for (let i = asciiOne + 1; i < asciiTwo; i++) {
     		result += ` ${String.fromCharCode(i)}`
     	}
+        
+    	console.log(result)
     } else {
     	for (let i = asciiTwo + 1; i < asciiOne; i++) {
-    		result += ` ${String.fromCharCode(i)}`
+    		arr.push(String.fromCharCode(i))
     	}
+        
+    	console.log(arr.join(' '))
     }
-    
-    console.log(result)
+}      
    
-}   
-   
-charRange('c', 'n') 
+charRange('c', '#') 
