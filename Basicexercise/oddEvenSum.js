3d73a0d95be7b9d724deeb4572e0a26c1f77fb2c
PavelMetodiev <48319741+PavelMetodiev@users.noreply.github.com>
2019-05-03 15:38:22 +0300

    Update oddEvenSum.js

--- Basicexercise/oddEvenSum.js
+++ Basicexercise/oddEvenSum.js
@@ -1,12 +1,12 @@
 function oddEvenSum (input) {
-   var oddSum = 0 
-   var evenSum = 0		
+   let oddSum = 0 
+   let evenSum = 0		
    let stringInput = input.toString() 
     
-   for (let i = 0; i < stringInput.length; i++) { 
-   		+stringInput[i] % 2 == 0 ? evenSum += +stringInput[i] : oddSum += +stringInput[i];
+   for (let num of stringInput) { 
+   		+num % 2 == 0 ? evenSum += +num : oddSum += +num;
    }
-   
+    
    console.log(`Odd sum = ${oddSum}, Even sum = ${evenSum}`)
 }   
     
