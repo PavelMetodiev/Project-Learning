6427b2f7772cdf6f57687a8df224938be9e48617
PavelMetodiev <48319741+PavelMetodiev@users.noreply.github.com>
2019-05-08 16:21:42 +0300

    Create firstLastKnums.js

---
+++ Basicexercise/firstLastKnums.js
@@ -0,0 +1,11 @@
+function firstLastKnums(arr) {
+    
+    let kNum = arr.shift()
+    let firstK = arr.slice(0, kNum)
+    let secondK = arr.slice((arr.length - kNum), arr.length)
+    
+    console.log(firstK)
+    console.log(secondK)
+}    
+    
+firstLastKnums([3, 6, 7, 8, 9]) 
