1887f196ec2a87f781eae1d0bd1ed3bf373e293d
PavelMetodiev <48319741+PavelMetodiev@users.noreply.github.com>
2019-05-09 10:58:51 +0300

    Update seq.js

--- Basicexercise/seq.js
+++ Basicexercise/seq.js
@@ -1,26 +1,23 @@
 function lastKnumsSequence(arrLength, numsToSum) {
     
-    let sum = 0
     let start = 0
     let end = 0
     let arr = [1]
-    
-    
+   
     for (let current = 1; current < arrLength; current++) {
+    	let sum = 0
     
     	start = Math.max(0, current - numsToSum)
     	end = current - 1
-    	sum = arr[start + end]
-    	arr[current] = sum
     
-    console.log(start)
-    console.log(end)
-    console.log(sum)	
-    console.log(arr)
+   		for (let i = start; i <= end; i++) {
+    		sum += arr[i]
+        }
+    
+    	arr[current] = sum
     }
     
-    
-    
+    console.log(arr.join(' '))
 }    
     
-lastKnumsSequence(6, 3)
+lastKnumsSequence(9, 5)
