dec0394fa3623ebf9a06aa4903eaf5cc5136c0ae
Pavel Pavel <pavel.d.metodiev@gmail.com>
2019-05-24 16:46:24 +0300

    Working on the vectors

--- Projects/JavaScripterio/segment.js
+++ Projects/JavaScripterio/segment.js
@@ -1,9 +1,24 @@
 class Segment {
-    constructor(x, y, thickness){
+    constructor(x, y, thickness) {
         this.start = createVector(x, y);
         this.end = this.start.copy();
         this.thickness = thickness;
         this.angle = 0;
-        this.len = 5;
+        this.len = 30;
+    }
+
+    display(color) {
+        stroke(color.x, color.y, color.z)
+        strokeWeight(this.thickness)
+        line(this.start.x, this.start.y, this.end.x, this.end.y)
+        stroke(255)
+        strokeWeight(8)
+        point(this.start.x, this.start.y)
+    }
+
+    calculateEnd() {
+        let dx = cos(this.angle) * this.len
+        let dy = sin(this.angle) * this.len
+        this.end.set(this.start.x + dx, this.start.y + dy)
     }
 }
--- Projects/JavaScripterio/sketch.js
+++ Projects/JavaScripterio/sketch.js
@@ -1,6 +1,13 @@
+let segment
+let color
+
 function setup() {
     createCanvas(600, 600);
+    segment = new Segment(width / 2, height / 2, 20)
+    color = createVector(random(255), random(255), random(255))
 }
 function draw() {
     background(0);
+    segment.calculateEnd()
+    segment.display(color)
 }
