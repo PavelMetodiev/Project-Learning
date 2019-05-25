f0acad35504d413bf521a45cc49b79c646e04bc5
Pavel Pavel <pavel.d.metodiev@gmail.com>
2019-05-25 16:28:42 +0300

    Making the snake

--- Projects/JavaScripterio/sketch.js
+++ Projects/JavaScripterio/sketch.js
@@ -1,10 +1,12 @@
 let segment
 let color
+let snake
 
 function setup() {
     createCanvas(600, 600);
     segment = new Segment(width / 2, height / 2, 20)
     color = createVector(random(255), random(255), random(255))
+    snake = new Snake()
 }
 function draw() {
     background(0);
