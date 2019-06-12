d9acf3fb7dcf70d2150e26fc42d7078a44c14a44
pavelmetodiev <pavel.d.metodiev@gmail.com>
2019-06-12 10:52:21 +0000

    another check

--- Projects/JavaScripterio/segment.js
+++ Projects/JavaScripterio/segment.js
@@ -36,16 +36,16 @@ class Segment {
     drawEye () {
         stroke(255)
         strokeWeight(this.thickness / 2.2)
-        point(this.end.x + snake.head.thickness / 7, this.end.y + snake.head.thickness / 7)
+        point(this.end.x + snake.head.thickness / 5, this.end.y + snake.head.thickness / 5)
         stroke(0)
         strokeWeight(this.thickness / 5)
-        point(this.end.x + snake.head.thickness / 7, this.end.y + snake.head.thickness / 7)
+        point(this.end.x + snake.head.thickness / 5, this.end.y + snake.head.thickness / 5)
 
         stroke(255)
         strokeWeight(this.thickness / 2.2)
-        point(this.end.x - snake.head.thickness / 7, this.end.y - snake.head.thickness / 7)
+        point(this.end.x - snake.head.thickness / 5, this.end.y - snake.head.thickness / 5)
         stroke(0)
         strokeWeight(this.thickness / 5)
-        point(this.end.x - snake.head.thickness / 7, this.end.y - snake.head.thickness / 7)
+        point(this.end.x - snake.head.thickness / 5, this.end.y - snake.head.thickness / 5)
     }
 }
