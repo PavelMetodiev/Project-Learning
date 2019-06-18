fdf67585f614ae01aa8d0f231a720c1c99f161aa
PavelMetodiev <48319741+PavelMetodiev@users.noreply.github.com>
2019-06-18 14:25:13 +0300

    Formats the given text into paragraphs.
    Formats the given text into paragraphs with 3 sentances.

--- Projects/Format the Text/solution.js
+++ Projects/Format the Text/solution.js
@@ -1,3 +1,33 @@
 function solve() {
-  //TODO...
+  let textToPocess = document.getElementById('input').textContent
+
+  let sentances = textToPocess.split('.')
+  let pharagraph = []
+  let sentance = ''
+
+  while (sentances.length > 3) {
+    pharagraph = []
+    for (let i = 0; i < 3; i++) {
+      sentance = sentances.shift()
+      pharagraph.push(sentance)
+    }
+    pharagraph.push('')
+    let p = document.createElement('p')
+    p.id = "koleda"
+    p.innerHTML = pharagraph.join('.')
+    output.appendChild(p)
+  }
+
+  pharagraph = []
+  for (let i = 0; i < sentances.length; i++) {
+    sentance = sentances.shift()
+    pharagraph.push(sentance)
+  }
+  pharagraph.push('')
+  let p = document.createElement('p')
+  p.id = "koleda"
+  p.innerHTML = pharagraph.join('.')
+  output.appendChild(p)
 }
+
+
