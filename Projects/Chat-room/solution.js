e1e31e4d84d85f283c28ebe8a330f29978b41605
PavelMetodiev <48319741+PavelMetodiev@users.noreply.github.com>
2019-06-26 17:24:01 +0300

    Add files via upload

--- Projects/Chat-room/solution.js
+++ Projects/Chat-room/solution.js
@@ -1,28 +1,23 @@
 function solve() {
-    let count = 0
-    let repeats = '\n'.repeat(count)
+
     document.getElementById('send').addEventListener("click", mouseClicking)
     document.getElementById('chat_input').addEventListener("keyup", keyClicking)
 
+    function mouseClicking() {
+        createDiv()
+    }
+    function keyClicking(event) {
+        if (event.keyCode === 13) {
+            createDiv()
+        }
+    }
     function createDiv() {
         if (document.getElementById('chat_input').value !== '' && document.getElementById('chat_input').value !== '\n') {
             let newMessage = document.createElement('div')
             newMessage.innerText = document.getElementById('chat_input').value
             newMessage.setAttribute('class', 'message my-message');
             document.getElementById("chat_messages").appendChild(newMessage)
-            document.getElementById('chat_input').value = ''
-            console.log(repeats)
-        } else {
-            document.getElementById('chat_input').value = ''
         }
-    }
-    function mouseClicking() {
-        createDiv()
-    }
-    function keyClicking(event) {
-        count++
-        if (event.keyCode === 13) {
-            createDiv()
-        }
+        document.getElementById('chat_input').value = ''
     }
 }
