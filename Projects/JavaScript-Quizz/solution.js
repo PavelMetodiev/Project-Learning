5a245bb78402439be3c143b9079fdfff571f949c
PavelMetodiev <48319741+PavelMetodiev@users.noreply.github.com>
2019-07-04 13:14:08 +0300

    Add files via upload

--- Projects/JavaScript-Quizz/solution.js
+++ Projects/JavaScript-Quizz/solution.js
@@ -1,5 +1,24 @@
 function solve() {
+    let score = 0
+    var questionNum = 0
+    var qestion1 = ''
+    var qestion2 = ''
 
-    
+    document.getElementById(`correct${questionNum + 1}`).addEventListener("click", correctAnswer)
+    function correctAnswer() {
+        score += 1
+        questionNum += 1
+
+        qestion1 = document.getElementById(`q${questionNum}`)
+
+        qestion1.setAttribute('class', 'hidden');
+
+        qestion2 = document.getElementById(`q${questionNum + 1}`)
+
+        qestion2.setAttribute('class', '');
+
+        console.log(questionNum, score)
+    }
+
 
 }
