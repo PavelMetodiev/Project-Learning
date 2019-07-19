82355fa1eb1e2ad04729804a29148fdbbbd92d91
PavelMetodiev <48319741+PavelMetodiev@users.noreply.github.com>
2019-07-19 11:23:21 +0300

    Working on it

--- Projects/JavaScript-Quizz/solution.js
+++ Projects/JavaScript-Quizz/solution.js
@@ -1,24 +1,44 @@
 function solve() {
     let score = 0
-    var questionNum = 0
-    var qestion1 = ''
-    var qestion2 = ''
+    var questionNum = 1
 
-    document.getElementById(`correct${questionNum + 1}`).addEventListener("click", correctAnswer)
     function correctAnswer() {
         score += 1
+        document.getElementById(`q${questionNum}`)
+        document.getElementById(`q${questionNum}`).setAttribute('class', 'hidden');
+        let check = document.getElementById(`q${questionNum + 1}`)
+        if (check !== document.getElementById('q4')) {
+            document.getElementById(`q${questionNum + 1}`).setAttribute('class', '');
+        }
+        if (score >= 3) {
+            document.getElementById('champ').setAttribute('class', '');
+            //document.getElementById('results').setAttribute('class', '');
+            console.log('Ole')
+        }
         questionNum += 1
-
-        qestion1 = document.getElementById(`q${questionNum}`)
-
-        qestion1.setAttribute('class', 'hidden');
-
-        qestion2 = document.getElementById(`q${questionNum + 1}`)
-
-        qestion2.setAttribute('class', '');
-
-        console.log(questionNum, score)
+        console.log(score)
+        console.log(questionNum)
+        
     }
+    document.getElementById('correct1').addEventListener("click", correctAnswer)
+    document.getElementById('correct2').addEventListener("click", correctAnswer)
+    document.getElementById('correct3').addEventListener("click", correctAnswer)
 
+    function wrongAnswer() {
+        document.getElementById(`q${questionNum}`)
+        document.getElementById(`q${questionNum}`).setAttribute('class', 'hidden');
+        let check = document.getElementById(`q${questionNum + 1}`)
+        if (check !== document.getElementById('q4')) {
+            document.getElementById(`q${questionNum + 1}`).setAttribute('class', '');
+        } else {
+            document.getElementById('champ').setAttribute('class', '');
+        }
+        questionNum += 1
+        console.log(score)
+        console.log(questionNum)
+    }
+    document.getElementById('wrong1').addEventListener("click", wrongAnswer)
+    document.getElementById('wrong2').addEventListener("click", wrongAnswer)
+    document.getElementById('wrong3').addEventListener("click", wrongAnswer)
 
 }
