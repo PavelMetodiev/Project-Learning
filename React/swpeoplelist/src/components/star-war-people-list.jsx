a873ad63bf4a5bc076a8afc0842b0f9bf5f26768
PavelMetodiev <pavel.d.metodiev@gmail.com>
2019-10-16 07:46:48 +0300

    StarWarsList

--- React/swpeoplelist/src/components/star-war-people-list.jsx
+++ React/swpeoplelist/src/components/star-war-people-list.jsx
@@ -2,12 +2,22 @@ import React from 'react';
 
 class StarWarsPeopleList extends React.Component {
     state = {
-        people: []
-    } 
+        people: [],
+        isLoading: true,
+        error: false,
+    }; 
 
     render() {
-        const { people } = this.state;
+        const { people, isLoading, error } = this.state;
         
+        if (error) {
+            return <span> Smt went wrong, pls reload</span>;
+        }
+
+        if (isLoading) {
+            return <span>Loading....</span>
+        }
+
         return (
             <ul>
                 {
@@ -21,6 +31,20 @@ class StarWarsPeopleList extends React.Component {
             </ul>
         )
     }
+
+    componentDidMount() {
+        fetch('https://swapi.co/api/people/')
+        .then(res => res.json())
+        .then(data => data.results)
+        .then(people => this.setState({ people, isLoading: false }))
+        .catch(error => {
+            console.error(error);
+            this.setState({
+                error:true
+            });
+        });
+    }
+    
 }
 
 export default StarWarsPeopleList;
--- React/swpeoplelist/src/index.js
+++ React/swpeoplelist/src/index.js
@@ -5,14 +5,29 @@ import RandomList from './components/random-list';
 import StarWarsPeopleList from './components/star-war-people-list';
 
 class App extends React.Component {
+    state = {
+        randomList: [
+            1, 2, 3, 4
+        ]
+    }
+
     render() {
+        const { randomList } = this.state;
+
         return (
             <React.Fragment>
                 <StarWarsPeopleList />
-                <RandomList />
+                <RandomList randomList={randomList} />
             </React.Fragment>
         );
     }
+
+    componentDidMount() {
+        this.setState((prevState) => ({
+            randomList: [...prevState.randomList, 5]
+        }));
+    }
+    
 }
 
 ReactDOM.render(
